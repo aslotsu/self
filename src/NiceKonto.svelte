4fc0563639863161246bd6e8a204a78dbafeef57
Alfred <alfredsklotsu@gmail.com>
2023-08-08 11:58:07 +0000

    Initial commit

---
+++ src/NiceKonto.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+    let total = 1
+
+    export const getTotal = () => {
+        return total
+    }
+</script>
+<script>
+    let items = 1
+
+    function addToCart() {
+        items +=2
+        total+=2
+    }
+
+</script>
+
+
+<button on:click={addToCart}>Add me to Cart {items}</button>
---
+++ src/water.svelte
@@ -0,0 +1,5 @@
+<script>
+    import NiceKonto from "./NiceKonto.svelte";
+</script>
+
+<NiceKonto />
